dba32411b11ae85b61f3a39577ed400d948a24e5
radioactivearm <andy.mcrae21@gmail.com>
2026-06-24 12:57:19 -0400

    implemented complete command.

--- src/TaskOrg/main.py
+++ src/TaskOrg/main.py
@@ -1,6 +1,8 @@
 import json
+from typing import Any
+
 from objects import Task
-from functions import print_json, print_tasks, write_task, read_tasks
+from functions import back2_tasks, print_tasks, write_task, read_tasks
 import typer
 
 # json file path
@@ -17,6 +19,18 @@ def add(task: str, category: str = 'none', priority: int = 3, status: bool = Fal
 def tasks():
     tasks_json = read_tasks(file_path)
     print_tasks(tasks_json)
+
+@app.command()
+def complete(task: str):
+    with open(file_path, 'r') as file:
+        db = json.load(file)
+
+    for i in 0, len(db['tasks']) -1:
+        if db['tasks'][i]['name'] == task:
+            db['tasks'][i]['status'] = True
+
+    with open(file_path, 'w') as file:
+        json.dump(db, file)
 
 if __name__ == '__main__':
     app()
